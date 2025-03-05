013911fc5c1ddcc4bf9de6a9b20c8b2aeb4af600
vikasbagoria <vikasbagoria@gmail.com>
2025-03-05 17:09:58 +0530

    first commddfssdffit

--- src/FlagCard.js
+++ src/FlagCard.js
@@ -3,7 +3,7 @@ import "./FlagCard.css";
 
 const FlagCard = ({ country }) => {
   return (
-    <div className="countryCrd">
+    <div className="countryCard">
       <img src={country.png} alt={country.common ? `${country.common} flag` : "Country flag"} className="flag-image" />
       <span className="country-name">{country.common || "Unknown Country"}</span>
     </div>
